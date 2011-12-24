6a8415949550d24d64b4005e860e4dce23f6db11
lizbeth <lizbeth@lizbeth-desktop.(none)>
2011-12-25 03:37:55 +0800

    added individual unit viewing

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -14,7 +14,7 @@ class UnitsController < ApplicationController
   # GET /units/1.json
   def show
     @unit = Unit.find(params[:id])
-
+    @item = Item.find(@unit.item_id)
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @unit }
