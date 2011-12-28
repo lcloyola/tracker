53ffc538ca14e241d26c56989e0b6c102a74e682
lizbeth <lizbeth@lizbeth-desktop.(none)>
2011-12-29 03:03:43 +0800

    added viewing of available units and groups

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,10 @@ class EventsController < ApplicationController
   # GET /events/1.json
   def show
     @event = Event.find(params[:id])
-
+    #TODO: check date availability
+    @units = Unit.all()
+    @groups = Group.all()
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @event }
